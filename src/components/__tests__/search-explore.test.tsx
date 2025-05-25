43f5d3840bb9172c1a917472edc1882a8c1c46a1
StyleList94 <43318609+StyleList94@users.noreply.github.com>
2025-05-25 23:39:39 +0900

    feat: update search explore component

--- src/components/__tests__/search-explore.test.tsx
+++ src/components/__tests__/search-explore.test.tsx
@@ -17,6 +17,28 @@ describe('<SearchExplore />', () => {
     expect(screen.getByLabelText('검색어')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('검색하고 싶은 거')).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /search/ })).toBeInTheDocument();
+
+    expect(screen.getByLabelText('모두 선택')).toBeInTheDocument();
+
+    expect(screen.getByRole('heading', { name: '주제' })).toBeInTheDocument();
+    expect(screen.getByLabelText('문제 해결')).toBeInTheDocument();
+    expect(screen.getByLabelText('맛집')).toBeInTheDocument();
+    expect(screen.getByLabelText('실시간 가십')).toBeInTheDocument();
+
+    expect(screen.getByRole('heading', { name: '해외' })).toBeInTheDocument();
+    expect(screen.getByLabelText('Google')).toBeInTheDocument();
+    expect(screen.getByLabelText('Bing')).toBeInTheDocument();
+    expect(screen.getByLabelText('DuckDuckGo')).toBeInTheDocument();
+
+    expect(screen.getByRole('heading', { name: '국내' })).toBeInTheDocument();
+    expect(screen.getByLabelText('네이버')).toBeInTheDocument();
+    expect(screen.getByLabelText('다음')).toBeInTheDocument();
+    expect(screen.getByLabelText('네이트')).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('heading', { name: '커뮤니티' }),
+    ).toBeInTheDocument();
+    expect(screen.getByLabelText('디시인사이드')).toBeInTheDocument();
   });
 
   it('should be changed query', () => {
@@ -35,12 +57,97 @@ describe('<SearchExplore />', () => {
     render(<SearchExplore />);
 
     const inputElement = screen.getByPlaceholderText('검색하고 싶은 거');
+    const buttonElement = screen.getByRole('button', { name: /search/ });
+
+    expect(buttonElement).toBeDisabled();
 
     fireEvent.change(inputElement, { target: { value: 'fender' } });
-    fireEvent.click(screen.getByRole('button', { name: /search/ }));
 
+    expect(buttonElement).not.toBeDisabled();
+    fireEvent.click(buttonElement);
     expect(openMock).toBeCalledTimes(7);
 
+    const allCheckbox = screen.getByLabelText('모두 선택');
+    fireEvent.click(allCheckbox);
+    expect(buttonElement).toBeDisabled();
+
+    openMock.mockReset();
+    const problemCheckbox = screen.getByLabelText('문제 해결');
+    fireEvent.click(problemCheckbox);
+    expect(buttonElement).not.toBeDisabled();
+    fireEvent.click(buttonElement);
+    expect(openMock).toBeCalledTimes(3);
+
+    openMock.mockReset();
+    const foodCheckbox = screen.getByLabelText('맛집');
+    fireEvent.click(foodCheckbox);
+    fireEvent.click(buttonElement);
+    expect(openMock).toBeCalledTimes(5);
+
+    openMock.mockReset();
+    const communityCheckbox = screen.getByLabelText('실시간 가십');
+    fireEvent.click(communityCheckbox);
+    fireEvent.click(buttonElement);
+    expect(openMock).toBeCalledTimes(6);
+
     vi.unstubAllGlobals();
+  });
+
+  test('checkbox', () => {
+    render(<SearchExplore />);
+
+    const checkboxElement = screen.getByLabelText('Google');
+
+    expect(checkboxElement).toBeChecked();
+    fireEvent.click(checkboxElement);
+    expect(checkboxElement).not.toBeChecked();
+    fireEvent.click(checkboxElement);
+
+    const externalSearchName = ['Google', 'Bing', 'DuckDuckGo'];
+    const internalSearchName = ['네이버', '다음', '네이트'];
+    const communitySearchName = ['디시인사이드'];
+
+    const allCheckbox = screen.getByLabelText('모두 선택');
+    expect(allCheckbox).toBeChecked();
+    fireEvent.click(allCheckbox);
+    expect(allCheckbox).not.toBeChecked();
+
+    [
+      ...externalSearchName,
+      ...internalSearchName,
+      ...communitySearchName,
+    ].forEach((item) => {
+      expect(screen.getByLabelText(item)).not.toBeChecked();
+    });
+
+    const problemCheckbox = screen.getByLabelText('문제 해결');
+    expect(problemCheckbox).not.toBeChecked();
+    fireEvent.click(problemCheckbox);
+    expect(problemCheckbox).toBeChecked();
+
+    externalSearchName.forEach((item) => {
+      expect(screen.getByLabelText(item)).toBeChecked();
+    });
+
+    const foodCheckbox = screen.getByLabelText('맛집');
+    expect(foodCheckbox).not.toBeChecked();
+    fireEvent.click(foodCheckbox);
+    expect(foodCheckbox).toBeChecked();
+
+    internalSearchName.slice(0, 2).forEach((item) => {
+      expect(screen.getByLabelText(item)).toBeChecked();
+    });
+
+    const communityCheckbox = screen.getByLabelText('실시간 가십');
+    expect(communityCheckbox).not.toBeChecked();
+    fireEvent.click(communityCheckbox);
+    expect(communityCheckbox).toBeChecked();
+
+    communitySearchName.slice(0, 2).forEach((item) => {
+      expect(screen.getByLabelText(item)).toBeChecked();
+    });
+
+    fireEvent.click(screen.getByLabelText('네이트'));
+    expect(allCheckbox).toBeChecked();
   });
 });
--- src/components/search-explore.tsx
+++ src/components/search-explore.tsx
@@ -1,81 +1,236 @@
 'use client';
 
-import { type ChangeEvent, useRef, useState } from 'react';
+import {
+  type ChangeEvent,
+  type ComponentProps,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { SearchIcon } from 'lucide-react';
 import { toast } from 'sonner';
 
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
 import Input from '@/components/ui/input';
 import Label from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
-import { SearchIcon } from 'lucide-react';
+import Checkbox from '@/components/ui/checkbox';
+import Separator from '@/components/ui/separator';
+
+type CheckboxWithLabelProps = {
+  labelText: string;
+} & ComponentProps<typeof CheckboxPrimitive.Root>;
 
 const searchEngines = [
   {
     id: 'google',
+    name: 'Google',
     url: (keyword: string) =>
       `https://www.google.com/search?q=${encodeURIComponent(keyword)}`,
   },
   {
     id: 'naver',
+    name: '네이버',
     url: (keyword: string) =>
       `https://search.naver.com/search.naver?query=${encodeURIComponent(keyword)}`,
   },
   {
     id: 'daum',
+    name: '다음',
     url: (keyword: string) =>
       `https://search.daum.net/search?q=${encodeURIComponent(keyword)}`,
   },
   {
     id: 'nate',
+    name: '네이트',
     url: (keyword: string) =>
       `https://search.daum.net/nate?q=${encodeURIComponent(keyword)}`,
   },
   {
     id: 'bing',
+    name: 'Bing',
     url: (keyword: string) =>
       `https://www.bing.com/search?q=${encodeURIComponent(keyword)}`,
   },
   {
     id: 'duckduckgo',
+    name: 'DuckDuckGo',
     url: (keyword: string) =>
       `https://duckduckgo.com/?q=${encodeURIComponent(keyword)}`,
   },
   {
     id: 'dcinside',
+    name: '디시인사이드',
     url: (keyword: string) =>
       `https://search.dcinside.com/combine/q/${encodeURIComponent(keyword)}`,
   },
 ];
 
+const externalSearchEngines = searchEngines
+  .filter((searchEngine) =>
+    ['google', 'bing', 'duckduckgo'].some(
+      (target) => target === searchEngine.id,
+    ),
+  )
+  .map(({ id, name }) => ({ id, name }));
+
+const internalSearchEngines = searchEngines
+  .filter((searchEngine) =>
+    ['naver', 'daum', 'nate'].some((target) => target === searchEngine.id),
+  )
+  .map(({ id, name }) => ({ id, name }));
+
+const communitySearchEngines = searchEngines
+  .filter((searchEngine) =>
+    ['dcinside'].some((target) => target === searchEngine.id),
+  )
+  .map(({ id, name }) => ({ id, name }));
+
+const initialCheckboxState: Record<string, boolean> = searchEngines.reduce(
+  (prev, current) => ({
+    ...prev,
+    [current.id]: true,
+  }),
+  {},
+);
+
+const CheckboxWithLabel = ({ labelText, ...props }: CheckboxWithLabelProps) => (
+  <div className="flex items-center space-x-2">
+    <Checkbox {...props} />
+    <label
+      htmlFor={props.id}
+      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+    >
+      {labelText}
+    </label>
+  </div>
+);
+
 const SearchExplore = () => {
   const [query, setQuery] = useState('');
 
+  const [isChecked, setIsChecked] = useState({ ...initialCheckboxState });
+
   const toastId = useRef<string | number | null>(null);
+
+  const isAllChecked = useMemo(
+    () => Object.values(isChecked).every(Boolean),
+    [isChecked],
+  );
+
+  const isProblemChecked = useMemo(
+    () => externalSearchEngines.every((item) => isChecked[item.id]),
+    [isChecked],
+  );
+
+  const isFoodChecked = useMemo(
+    () => internalSearchEngines.slice(0, 2).every((item) => isChecked[item.id]),
+    [isChecked],
+  );
+
+  const isCommunityChecked = useMemo(
+    () => communitySearchEngines.every((item) => isChecked[item.id]),
+    [isChecked],
+  );
+
+  const isSearchable = useMemo(
+    () => Object.values(isChecked).some(Boolean),
+    [isChecked],
+  );
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
   };
 
-  const handleClickSearch = () => {
-    searchEngines.forEach((engine) => {
-      const popup = window.open(engine.url(query), `${engine.id}${Date.now()}`);
+  const handleCheck = (name: string) => (checked: boolean) => {
+    switch (name) {
+      case 'all':
+        setIsChecked(
+          searchEngines.reduce(
+            (prev, current) => ({
+              ...prev,
+              [current.id]: checked,
+            }),
+            {},
+          ),
+        );
+        return;
+      case 'googling':
+        setIsChecked((prevState) => {
+          const targetState = externalSearchEngines.reduce(
+            (prev, current) => ({
+              ...prev,
+              [current.id]: checked,
+            }),
+            {},
+          );
+          return { ...prevState, ...targetState };
+        });
+        return;
 
-      if (!popup || popup.closed || typeof popup.closed === 'undefined') {
-        if (toastId.current) {
-          toast.dismiss(toastId.current);
-          toastId.current = null;
-        } else {
-          toastId.current = toast.warning('팝업이 차단됨!', {
-            position: 'bottom-right',
-            description: '검색 결과를 모두 확인하려면 팝업 해제를 해야합니다!',
-            duration: 3000,
-          });
+      case 'food':
+        setIsChecked((prevState) => {
+          const targetState = internalSearchEngines.slice(0, 2).reduce(
+            (prev, current) => ({
+              ...prev,
+              [current.id]: checked,
+            }),
+            {},
+          );
+          return { ...prevState, ...targetState };
+        });
+        return;
+
+      case 'community':
+        setIsChecked((prevState) => {
+          const targetState = communitySearchEngines.reduce(
+            (prev, current) => ({
+              ...prev,
+              [current.id]: checked,
+            }),
+            {},
+          );
+          return { ...prevState, ...targetState };
+        });
+        return;
+
+      default:
+        setIsChecked((prevState) => ({ ...prevState, [name]: checked }));
+    }
+  };
+
+  const handleClickSearch = () => {
+    if (!isSearchable) {
+      return;
+    }
+
+    searchEngines.forEach((engine) => {
+      if (isChecked[engine.id]) {
+        const popup = window.open(
+          engine.url(query),
+          `${engine.id}${Date.now()}`,
+        );
+
+        if (!popup || popup.closed || typeof popup.closed === 'undefined') {
+          if (toastId.current) {
+            toast.dismiss(toastId.current);
+            toastId.current = null;
+          } else {
+            toastId.current = toast.warning('팝업이 차단됨!', {
+              position: 'bottom-right',
+              description:
+                '검색 결과를 모두 확인하려면 팝업 해제를 해야합니다!',
+              duration: 3000,
+            });
+          }
         }
       }
     });
@@ -100,7 +255,7 @@ const SearchExplore = () => {
             <Button
               size="icon"
               variant="outline"
-              disabled={!query}
+              disabled={!query || !isSearchable}
               onClick={handleClickSearch}
               aria-label="search"
             >
@@ -109,6 +264,98 @@ const SearchExplore = () => {
           </div>
         </div>
       </CardContent>
+      <CardFooter className="@container flex flex-col items-start gap-4">
+        <div className="flex flex-col gap-4 w-full">
+          <CheckboxWithLabel
+            labelText="모두 선택"
+            id="all"
+            name="all"
+            checked={isAllChecked}
+            onCheckedChange={handleCheck('all')}
+          />
+
+          <Separator />
+
+          <section className="flex flex-col gap-2">
+            <h2 className="text-sm text-zinc-500 dark:text-zinc-400">주제</h2>
+            <div className="flex flex-col gap-2 @3xs:flex-row @2xs:items-center">
+              <CheckboxWithLabel
+                labelText="문제 해결"
+                id="googling"
+                name="googling"
+                checked={isProblemChecked}
+                onCheckedChange={handleCheck('googling')}
+              />
+              <CheckboxWithLabel
+                labelText="맛집"
+                id="food"
+                name="food"
+                checked={isFoodChecked}
+                onCheckedChange={handleCheck('food')}
+              />
+              <CheckboxWithLabel
+                labelText="실시간 가십"
+                id="community"
+                name="community"
+                checked={isCommunityChecked}
+                onCheckedChange={handleCheck('community')}
+              />
+            </div>
+          </section>
+        </div>
+
+        <Separator />
+
+        <div className="flex flex-col gap-3">
+          <section className="flex flex-col gap-2">
+            <h2 className="text-sm text-zinc-500 dark:text-zinc-400">해외</h2>
+            <div className="flex flex-col gap-2 @3xs:flex-row @2xs:items-center">
+              {externalSearchEngines.map((item) => (
+                <CheckboxWithLabel
+                  key={item.id}
+                  labelText={item.name}
+                  id={item.id}
+                  name={item.id}
+                  onCheckedChange={handleCheck(item.id)}
+                  checked={isChecked[item.id]}
+                />
+              ))}
+            </div>
+          </section>
+          <section className="flex flex-col gap-2">
+            <h2 className="text-sm text-zinc-500 dark:text-zinc-400">국내</h2>
+            <div className="flex flex-col gap-2 @3xs:flex-row @2xs:items-center">
+              {internalSearchEngines.map((item) => (
+                <CheckboxWithLabel
+                  key={item.id}
+                  labelText={item.name}
+                  id={item.id}
+                  name={item.id}
+                  onCheckedChange={handleCheck(item.id)}
+                  checked={isChecked[item.id]}
+                />
+              ))}
+            </div>
+          </section>
+          <section className="flex flex-col gap-2">
+            <h2 className="text-sm text-zinc-500 dark:text-zinc-400">
+              커뮤니티
+            </h2>
+            <div className="flex flex-col gap-2 @3xs:flex-row @2xs:items-center">
+              {communitySearchEngines.map((item) => (
+                <CheckboxWithLabel
+                  key={item.id}
+                  labelText={item.name}
+                  id={item.id}
+                  name={item.id}
+                  onCheckedChange={handleCheck(item.id)}
+                  checked={isChecked[item.id]}
+                />
+              ))}
+            </div>
+          </section>
+        </div>
+      </CardFooter>
     </Card>
   );
 };
