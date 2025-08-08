8500800bdd3e8099c75bc26d09c1df9ad07c2841
StyleList94 <43318609+StyleList94@users.noreply.github.com>
2025-08-08 20:25:09 +0900

    feat: add ESLint

---
+++ astro-hara/eslint.config.mjs
@@ -0,0 +1,41 @@
+import tseslint from 'typescript-eslint';
+import { configs as eslintPluginAstroConfigs } from 'eslint-plugin-astro';
+import stylish from 'eslint-config-stylish';
+import stylishReact from 'eslint-config-stylish/react';
+import stylishReactHooks from 'eslint-config-stylish/react-hooks';
+import stylishTypeScript from 'eslint-config-stylish/typescript';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default tseslint.config(
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    extends: [stylish],
+    rules: {
+      'import/prefer-default-export': 'off',
+    },
+  },
+  {
+    files: ['**/*.tsx'],
+    extends: [stylishReact],
+  },
+  {
+    files: ['**/use*.ts'],
+    extends: [stylishReactHooks],
+  },
+  {
+    files: ['**/*.{ts,mts,cts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+      },
+    },
+    extends: [stylishTypeScript],
+  },
+  ...eslintPluginAstroConfigs.recommended,
+  eslintConfigPrettier,
+);
