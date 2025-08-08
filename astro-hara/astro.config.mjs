6f6f681a424e8362bfe6f79585c9f871ccbf6f16
StyleList94 <43318609+StyleList94@users.noreply.github.com>
2025-08-08 22:07:45 +0900

    feat: add fonts

--- astro-hara/astro.config.mjs
+++ astro-hara/astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
@@ -9,5 +9,59 @@ export default defineConfig({
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: 'local',
+        name: 'Pretendard',
+        cssVariable: '--font-pretendard',
+        variants: [
+          {
+            weight: 400,
+            style: 'normal',
+            src: [
+              './src/assets/fonts/Pretendard/Pretendard-Regular.subset.woff2',
+            ],
+          },
+          {
+            weight: 500,
+            style: 'normal',
+            src: [
+              './src/assets/fonts/Pretendard/Pretendard-Medium.subset.woff2',
+            ],
+          },
+          {
+            weight: 600,
+            style: 'normal',
+            src: [
+              './src/assets/fonts/Pretendard/Pretendard-SemiBold.subset.woff2',
+            ],
+          },
+          {
+            weight: 700,
+            style: 'normal',
+            src: ['./src/assets/fonts/Pretendard/Pretendard-Bold.subset.woff2'],
+          },
+          {
+            weight: 800,
+            style: 'normal',
+            src: [
+              './src/assets/fonts/Pretendard/Pretendard-ExtraBold.subset.woff2',
+            ],
+          },
+        ],
+      },
+      {
+        provider: fontProviders.google(),
+        name: 'Roboto Mono',
+        cssVariable: '--font-roboto-mono',
+      },
+      {
+        provider: fontProviders.google(),
+        name: 'Titillium Web',
+        cssVariable: '--font-titillium-web',
+      },
+    ],
   },
 });
