d48115989acac5a5c65132422149f7e3af4958d8
StyleList94 <43318609+StyleList94@users.noreply.github.com>
2025-08-10 23:54:05 +0900

    fix: resolve issue to lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import tseslint from 'typescript-eslint';
 import { globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import astroESLintParser from 'astro-eslint-parser';
+import tsESLintParser from '@typescript-eslint/parser';
 
+import { configs as astroConfigs } from 'eslint-plugin-astro';
 import stylish from 'eslint-config-stylish';
 import stylishReact from 'eslint-config-stylish/react';
 import stylishReactHooks from 'eslint-config-stylish/react-hooks';
@@ -10,9 +14,11 @@ import vitest from '@vitest/eslint-plugin';
 import testingLibrary from 'eslint-plugin-testing-library';
 
 export default tseslint.config(
-  globalIgnores(['.astro']),
+  globalIgnores(['node_modules', 'dist', '.astro']),
+  ...astroConfigs.recommended,
+  ...astroConfigs['jsx-a11y-strict'],
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx,astro}'],
     extends: [stylish],
     rules: {
       'import/prefer-default-export': 'off',
@@ -39,6 +45,27 @@ export default tseslint.config(
       },
     },
     extends: [stylishTypeScript],
+  },
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: astroESLintParser,
+      parserOptions: {
+        parser: tsESLintParser,
+        extraFileExtensions: ['.astro'],
+        project: './tsconfig.json',
+        ecmaVersion: 12,
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    extends: [stylishTypeScript],
+    rules: {
+      'astro/prefer-class-list-directive': 'warn',
+      'import/namespace': 'off',
+    },
   },
   {
     files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
