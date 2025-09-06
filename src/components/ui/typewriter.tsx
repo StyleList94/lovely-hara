cf1cd2be6cdfc281672e5543bb604a415c423e67
StyleList94 <43318609+StyleList94@users.noreply.github.com>
2025-09-07 00:20:18 +0900

    feat: update typwriter

--- src/components/ui/typewriter.tsx
+++ src/components/ui/typewriter.tsx
@@ -1,70 +1,38 @@
-import { motion, useMotionValue, useTransform, animate } from 'motion/react';
-import { useEffect, useState } from 'react';
+import { delay, wrap } from 'motion';
+import {
+  Typewriter as MotionTypewriter,
+  type TypingSpeed,
+} from 'motion-plus/react';
+import { useState, type CSSProperties } from 'react';
 
 type Props = {
   texts: string[];
-  speed?: number;
-  className?: string;
+  speed?: number | TypingSpeed;
 };
 
-const Typewriter = ({ texts, speed = 0.1, className = '' }: Props) => {
+const cursor: CSSProperties = {
+  width: '1.5rem',
+  height: '0.25rem',
+};
+
+const Typewriter = ({ texts, speed }: Props) => {
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
-  const [isTyping, setIsTyping] = useState(true);
-
-  const count = useMotionValue(0);
-  const rounded = useTransform(count, (latest) => Math.round(latest));
-  const displayText = useTransform(rounded, (latest) =>
-    texts[currentTextIndex].slice(0, latest),
-  );
-
-  useEffect(() => {
-    const currentText = texts[currentTextIndex];
-
-    if (isTyping) {
-      let timerId: NodeJS.Timeout;
-      const controls = animate(count, currentText.length, {
-        type: 'tween',
-        duration: currentText.length * speed,
-        ease: 'linear',
-        onComplete: () => {
-          timerId = setTimeout(() => {
-            animate(count, 0, {
-              duration: currentText.length * speed * 0.5,
-              ease: 'linear',
-              onComplete: () => {
-                setCurrentTextIndex((prev) => (prev + 1) % texts.length);
-                setIsTyping(true);
-              },
-            });
-          }, 4000);
-        },
-        onStop: () => {
-          clearTimeout(timerId);
-        },
-      });
-      return () => {
-        controls.stop();
-      };
-    }
-
-    return () => {};
-  }, [count, currentTextIndex, texts, speed, isTyping]);
 
   return (
-    <span className={className}>
-      <motion.span>{displayText}</motion.span>
-      <motion.span
-        animate={{ opacity: [1, 0] }}
-        transition={{
-          duration: 0.94,
-          repeat: Infinity,
-          ease: 'anticipate',
-          repeatType: 'reverse',
-        }}
-      >
-        _
-      </motion.span>
-    </span>
+    <MotionTypewriter
+      as="span"
+      cursorStyle={cursor}
+      onComplete={() => {
+        delay(
+          () =>
+            setCurrentTextIndex(wrap(0, texts.length, currentTextIndex + 1)),
+          2,
+        );
+      }}
+      speed={speed}
+    >
+      {texts[currentTextIndex]}
+    </MotionTypewriter>
   );
 };
 
